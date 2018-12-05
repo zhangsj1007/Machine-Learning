772dcbfa177637b8c3bb6445f34d53c478e55e9f
songjian zhang <songjianzhang@songjiandeiMac.local>
2018-12-05 10:46:46 +0800

    dbscan cluster

---
+++ dbscan/DBScan.java
@@ -0,0 +1,72 @@
+package com.sogou.map.kpi_monitor.monitor.algorithm;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.sogou.map.kpi_monitor.monitor.algorithm.DBScanPath.DBScanLink;
+ 
+public class DBScan {
+	private static double _calDistance(DBScanPath path1, DBScanPath path2) {
+		Set<Integer> linkSet = new HashSet<Integer>();
+		int commonLenths = 0;
+		List<DBScanLink> path1Links = path1.getLinks();
+		List<DBScanLink> path2Links = path2.getLinks();
+		for (DBScanLink link : path1Links) {
+			linkSet.add(link.getLinkId());
+		}
+		for (DBScanLink link : path2Links) {
+			if (linkSet.contains(link.getLinkId())) {
+				commonLenths += link.getLength();
+			}
+		}
+		
+		return 1 - Double.min((double)commonLenths / path1.getTotalLengths(), (double)commonLenths / path2.getTotalLengths());
+	}
+    
+	public static void cluster(double eps, int minPaths, List<DBScanPath> paths){
+		int cluster = 0;
+		for (DBScanPath path : paths) {
+			if (path.isVisited()) {
+				continue;
+			}
+			path.setVisited(true);
+			List<DBScanPath> neighPaths = _regionQuery(eps, path, paths);
+			if (neighPaths.size() < minPaths) {
+				path.setNoise(true);
+			}else {
+				path.setCluster(cluster);
+				for (int i = 0; i < neighPaths.size(); i++) {
+					DBScanPath neighPath = neighPaths.get(i);
+					if (!neighPath.isVisited()) {
+						neighPath.setVisited(true);
+						List<DBScanPath> neighNeighPaths = _regionQuery(eps, neighPath, paths);
+						if (neighNeighPaths.size() >= minPaths) {
+							neighPaths.addAll(neighNeighPaths);
+						}
+					}
+					
+					if (neighPath.getCluster() == -1) {
+						neighPath.setCluster(cluster);
+					}
+					if (neighPath.isNoise()) {
+						neighPath.setNoise(false);
+					}
+				}
+				cluster++;
+			}
+		}
+	}
+	
+	private static List<DBScanPath> _regionQuery(double eps, DBScanPath corePath, List<DBScanPath> paths){
+		List<DBScanPath> regionPaths = new ArrayList<DBScanPath>();
+		for (DBScanPath neighPath : paths) {
+			double d = _calDistance(corePath, neighPath);
+			if (d < eps) {
+				regionPaths.add(neighPath);
+			}
+		}
+		return regionPaths;
+	}
+}
---
+++ dbscan/DBScanPath.java
@@ -0,0 +1,106 @@
+package com.sogou.map.kpi_monitor.monitor.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import com.sogou.map.kpi_monitor.data.CorporateName;
+
+
+public class DBScanPath{
+		//Link
+		public class DBScanLink{
+			public DBScanLink(int linkId , int length) {
+				_linkId = linkId;
+				_length = length;
+			}
+			public int getLinkId() {
+				return _linkId;
+			}
+			public void setLinkId(int linkId) {
+				_linkId = linkId;
+			}
+			public int getLength() {
+				return _length;
+			}
+			public void setLength(int length) {
+				_length = length;
+			}
+			private int _linkId;
+			private int _length;
+		}
+		
+		public DBScanPath(CorporateName corporateName, int indexInOrigin) {
+			_links = new ArrayList<DBScanLink>();
+			_corporateName = corporateName;
+			_indexInOrigin = indexInOrigin;
+		}
+		
+		int getTotalLengths() {
+			if (_sumLengths > -1) {
+				return _sumLengths;
+			}
+			_sumLengths = 0;
+			for (DBScanLink link : _links) {
+				_sumLengths += link.getLength();
+			}
+			return _sumLengths;
+		}
+		
+		public boolean isVisited() {
+			return _visited;
+		}
+		
+		public void setVisited(boolean visited) {
+			_visited = visited;
+		}
+		
+		public boolean isNoise() {
+			return _noise;
+		}
+		
+		public void setNoise(boolean noise) {
+			_noise = noise;
+		}
+		
+		public int getCluster() {
+			return _cluster;
+		}
+		
+		public void setCluster(int cluster) {
+			_cluster = cluster;
+		}
+		
+		public void addDBScanLink(DBScanLink link) {
+			_links.add(link);
+		}
+		
+		public List<DBScanLink> getLinks(){
+			return _links;
+		}
+		
+		public CorporateName getCorporateName() {
+			return _corporateName;
+		}
+		
+		public void setCorporateName(CorporateName corporateName) {
+			_corporateName = corporateName;
+		}
+		
+		public int getindexInOrigin() {
+			return _indexInOrigin;
+		}
+		
+		public void setIndexInOrigin(int indexInOrigin) {
+			_indexInOrigin = indexInOrigin;
+		}
+		
+		private boolean _visited = false;
+		private boolean _noise = false;
+		private int _sumLengths = -1;
+		private int _cluster = -1;
+		private List<DBScanLink> _links;
+		private CorporateName _corporateName;
+		private int _indexInOrigin;
+	}
